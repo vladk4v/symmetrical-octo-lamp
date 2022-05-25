30d3ac2008fa5b1d6c81f41033731ef93885ecd8
Vladimir <vlad207702@gmail.com>
2022-05-25 19:13:27 +0600

    First change of text

--- src/main/java/ru/netology/JvmComprehension.java
+++ src/main/java/ru/netology/JvmComprehension.java
@@ -1,62 +1,93 @@
 package ru.netology;
 
-
-/**
- * Hello world!
- */
 public class JvmComprehension {
 	/**
-	 * class_loading "JvmComprehension"
-	 * загрузка класса, класс в metaspace
-	 *
+	 * Загрузка класса JvmComprehension
+	 * JvmComprehension.class в Metaspace
 	 *
 	 */
 
 	public static void main(String[] args) {
 		/**
-		 * создание фрейма метода "main"
+		 * Внутри фрейма для метода "main"
+		 *
 		 * */
 
 
 		int i = 1;// 1
 		/**
-		 * создание примитива, присвоение значения
-		 * загрузить во фрейм/стек
+		 * iconst_1
+		 * Загрузка константы типа "int" в "operand stack" (фрейм)
+		 *
+		 * istore_1
+		 * Сохранение значения из "operand stack" в "local variable" (фрейм)
+ 		 *
 		 */
 
 		Object o = new Object();// 2
 		/**
-		 * загрузить клас "Object"
-		 * класс в heap
+		 * invokespecial (java/lang/Object)
+		 * Вызов метода инициализации нового экземпляра объекта "java.lang.Object" (new).
+		 * Получение ссылки (на экземпляр объекта)
 		 *
-		 * создание экземпляра класса, связывание
+		 *
+		 * Экземпляр класса "Object" - в "heap"
+		 *
+		 * astore_2
+		 * Сохранение ссылки из "operand stack" в "local variable" (фрейм)
 		 *
 		 */
 
 		Integer ii = 2;	// 3
 		/**
-		 * загрузить класс "Integer"
-		 * класс в heap
+		 * iconst_2
+		 * Загрузка константы типа "int" в "operand stack" (фрейм)
 		 *
-		 * создание экземпляра класса "ii", присвоение значения, связывание
-		 * "ii" в стек/фрейм
+		 * invokestatic (java/lang/Integer.valueOf)
+		 * Вызов метода "valueOf" класса "Integer". Получение ссылки (на экземпляр объекта)
+		 *
+		 *
+		 * Экземпляр класса "Integer" - в "heap"
+		 *
+		 * astore_3
+		 * Сохранение ссылки из "operand stack" в "local variable" (фрейм)
 		 *
 		 */
 
 		printAll(o, i, ii);	// 4
 		/**
-		 * создание фрейма "printAll"
+		 * aload_2
+		 * Загрузка "local variable" (ссылки на экземпляр объекта) в "operand stack" ("о") (фрейм)
+		 *
+		 * iload_1
+		 * Загрузка "local variable" (примитива типа int) в "operand stack" ("i") (фрейм)
+		 *
+		 * aload_3
+		 * Загрузка "local variable" (ссылки на экземпляр объекта) в "operand stack" ("ii") (фрейм)
+		 *
+		 * invokestatic (ru/netology/JvmComprehension.printAll)
+		 * Вызов метода "printAll" класса "JvmComprehension"
+		 *
+		 *
+		 * Создание фрейма для этого метода
+		 *
 		 */
 
 		System.out.println("finished"); // 7
 		/**
 		 *
-		 * загрузить класс "System"
-		 * загрузить класс "String"
-		 * классы в heap
+		 * ldc (String "finished")
+		 * Загрузка значения типа "String" ("finished") из "constant pool" класса "JvmComprehension"
 		 *
-		 * 		Создание фрейма для метода "println"
-		 * 		передача значения "finished"
+		 * invokevirtual (java/io/PrintStream.println)
+		 * Вызов метода "println" класса "PrintStream"
+		 *
+		 *
+		 * Создание фрейма для этого метода
+		 *
+		 * return
+		 *
+		 *
 		 *
 		 */
 
@@ -64,30 +95,62 @@ public class JvmComprehension {
 
 	private static void printAll(Object o, int i, Integer ii) {
 		/**
-		 * Создать фрейм для метода "printALL"
-		 * загрузить во фрейм "о", "i", "ii"
+		 *
+		 * Внутри фрейма для метода "printAll"
 		 *
 		*/
 		Integer uselessVar = 700;
 		/**
-		 * загрузить класс "Integer"
-		 * загрузить ао фрейм метода "printAll" значение переменной "uselessVar" класса "Integer"
-		 * эта переменная будет будет объектом внимания GC, т.к. не используется
+		 *
+		 *
+		 * Загрузка константы типа "int" в "operand stack" (фрейм)
+		 *
+		 *
+		 * Вызов метода "valueOf" класса "Integer". Получение ссылки (на экземпляр объекта)
+		 *
+		 *
+		 * Экземпляр класса "Integer" - в "heap"
+		 *
+		 *
+		 * Сохранение ссылки из "operand stack" в "local variable" (фрейм)
+		 *
+		 * Будет являться объектом внимания GC, т.к. не используется
 		 *
 		 */
 
 		System.out.println(o.toString() + i + ii); // 6
 		/**
-		 * загрузить класс "System"
 		 *
-		 * Создание фрейма для метода "println"
 		 *
-		 * 		Создание фрейма для метода "toString"
-		 * 		Загрузить во фрейм значение "id"(?) экземпляра "о" класса "Object"
-		 * 		Закрыть ("destroy" (?)) фрейм
+		 * Загрузка (получение?) "local variable" (ссылки на экземпляр объекта) в "operand stack" ("о")
 		 *
-		 * Загрузить значение примитива "i"
-		 * Загрузить значение экземпляра "ii" класса "Integer"
+		 * Methodref (java/lang/Object.toString)
+		 * Вызов метода "toString" класса "String"
+		 *
+		 * Создание фрейма для этого метода
+		 *
+		 *
+		 * 		Внутри фрейма для "toString"
+		 *
+		 *
+		 * 		Возврат строкового значения экземпляра объекта (имя класса + @ + хэшкод)
+		 *
+		 *
+		 * 		Закрытие фрейма
+		 *
+		 *
+		 * Загрузка (получение?) "local variable" (примитива типа int) в "operand stack" ("i")
+		 *
+		 *
+		 * Загрузка (получение?) "local variable" (ссылки на экземпляр объекта) в "operand stack" ("ii")
+		 *
+		 *
+		 * Вызов метода "println" класса "PrintStream"
+		 *
+		 *
+		 * Создание фрейма для этого метода
+		 *
+		 * return
 		 *
 		 */
 
